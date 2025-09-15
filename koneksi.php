47222a1edf4a55fb9e0ccabe1efddce9b4cd8282
ANDIKA <andika061006@gmail.com>
2025-09-15 21:21:16 +0700

    two commit

--- koneksi.php
+++ koneksi.php
@@ -3,4 +3,4 @@ $host = "localhost";
 $user = "xirpl2-2"; // sesuaikan dengan user MySQL kamu
 $pass = "0093130960";     // sesuaikan dengan password MySQL kamu
 $db   = "db_xirpl2-2_1";
-$conn = mysqli_connect($host, $user, $pass, $db);
+$koneksi = mysqli_connect($host, $user, $pass, $db);
